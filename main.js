35b277754251ae5c8bfe8487f857a33883f63cbd
Leesha_1211 <leeshamohamedv@gmail.com>
2023-06-11 10:30:55 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+var typed=new Typed(".text", {
+    strings:["Software Engineer" , "Front-End Developer" , "competitive programmer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+
+});
+
+
